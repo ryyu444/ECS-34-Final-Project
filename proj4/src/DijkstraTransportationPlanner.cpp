d8862d3277d89e72f1e1effd2d9f823ac6e2c067
Raiyan <rsahmed1290@gmail.com>
2022-11-20 01:19:55 -0800

    implemented member variables in SImpl.

--- proj4/src/DijkstraTransportationPlanner.cpp
+++ proj4/src/DijkstraTransportationPlanner.cpp
@@ -1,5 +1,51 @@
 #include "DijkstraTransportationPlanner.h"
 
+struct DerivedConfiguration : public CTransportationPlanner::SConfiguration {
+    public:
+        std::shared_ptr<CStreetMap> m_streetMap;
+        std::shared_ptr<CBusSystem> m_busSystem;
+        double m_walkSpeed = 0.0;
+        double m_bikeSpeed = 0.0;
+        double m_defaultSpeedLimit = 0.0;
+        double m_busStopTime = 0.0;
+        double m_precomputeTime = 0.0;
+
+        DerivedConfiguration(std::shared_ptr<CStreetMap> streetMap, std::shared_ptr<CBusSystem> busSystem) {
+            m_streetMap = streetMap;
+            m_busSystem = busSystem;
+        };
+        
+        ~DerivedConfiguration() {};
+
+        std::shared_ptr<CStreetMap> StreetMap() const noexcept {
+            return m_streetMap;
+        };
+        
+        std::shared_ptr<CBusSystem> BusSystem() const noexcept {
+            return m_busSystem;
+        };
+        
+        double WalkSpeed() const noexcept {
+            return m_walkSpeed;
+        };
+        
+        double BikeSpeed() const noexcept {
+            return m_bikeSpeed;
+        };
+        
+        double DefaultSpeedLimit() const noexcept {
+            return m_defaultSpeedLimit;
+        };
+        
+        double BusStopTime() const noexcept {
+            return m_busStopTime;
+        };
+        
+        int PrecomputeTime() const noexcept {
+            return m_precomputeTime;
+        };
+};
+
 struct CDijkstraTransportationPlanner::SImplementation {
 
     public: 
